c814f66f37ef6d95291659ff20e6ae3a9054f724
girirahayu <info@girirahayu.id>
2026-08-17 10:59:17 +0700

    update copilot as default ai agent
    Signed-off-by: girirahayu <info@girirahayu.id>

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -158,7 +158,7 @@ Instruksi tambahan:
                     picker = "telescope",
                     auto_generate_title = true,
                     title_generation_opts = {
-                        adapter = nil,
+                        adapter = "copilot",
                         model = nil,
                         refresh_every_n_prompts = 0,
                         max_refreshes = 1,
