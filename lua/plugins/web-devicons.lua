eb322532bf1488b66912755dc250567d06c7dfc1
girirahayu <info@girirahayu.id>
2026-08-17 15:44:16 +0700

    handle clipboard remote
    Signed-off-by: girirahayu <info@girirahayu.id>

---
+++ lua/plugins/web-devicons.lua
@@ -0,0 +1,9 @@
+-- lua/plugins/web-devicons.lua
+-- Provider ikon visual untuk file-explorer, statusline, tab, & UI elements
+return {
+  "nvim-tree/nvim-web-devicons",
+  lazy = true,
+  opts = {
+    default = true,
+  },
+}
